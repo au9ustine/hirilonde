f218a7f53de452ee4d882822273a628b0c63d831
au9ustine <duke.augustine@gmail.com>
2017-01-08 00:38:58 +0800

    Add haproxy and confd

---
+++ README.md
@@ -0,0 +1,5 @@
+# Hirilonde
+
+[![Build Status](https://travis-ci.org/au9ustine/hirilonde.svg?branch=master)](https://travis-ci.org/au9ustine/hirilonde)
+
+up主今天太懒不想写文档
---
+++ makecert.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# This generated cert is only for dev only
+
+# Generate seeds for producing cryptographic random numbers
+dd if=/dev/urandom of=hirilonde.rnd bs=256 count=1
+
+# Generate certificate and keypair
+openssl req \
+        -new \
+        -x509 \
+        -days 3650 \
+        -rand hirilonde.rnd \
+        -config openssl.cnf \
+        -out hirilonde.pem \
+        -keyout hirilonde.pem \
+        -subj "/C=CN/ST=Shanghai/L=Shanghai/O=au9ustine/OU=DevOps/CN=localhost"
+openssl gendh 2048 >> hirilonde.pem
+
+# Destroy seeds
+rm -vf hirilonde.rnd
+
+# Display
+echo
+echo "Certificate details:"
+openssl x509 -subject -dates -fingerprint -noout -in hirilonde.pem
+echo
